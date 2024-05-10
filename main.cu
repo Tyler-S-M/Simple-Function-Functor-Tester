79be8d6a9105efa9f05237390b7f1ca204315fd1
Tyler Martin <146662714+Tyler-S-M@users.noreply.github.com>
2024-05-10 02:23:30 -0500

    Update main.cu
    added shared test

--- main.cu
+++ main.cu
@@ -22,14 +22,23 @@ class test_runner {
         int x = 1;
 
     public: 
+
+        //for testing functor state saving
         __device__ __noinline__ void operator()(float *d_data) {
-        int tid = threadIdx.x;
-        if (threadIdx.x == 0){
-            atomicAdd(&x, 1);
+            int tid = threadIdx.x;
+            if (threadIdx.x == 0){
+                atomicAdd(&x, 1);
+            }
+
+            OPERATION
         }
 
-        OPERATION
-    }
+        //for shared functor test
+        __device__ __noinline__ void shared(float *d_data) {
+            int tid = threadIdx.x;
+
+            OPERATION
+        }
 
 };
 
@@ -39,6 +48,9 @@ __constant__ test_runner* object_ptr = &object;
 
 //non-constant functor versions
 __device__ test_runner object_dev;
+
+//shared functor versions
+__shared__ test_runner object_shared;
 
 //functions that will either be inlined or not inlined
 __device__ __noinline__ void perform_calculations(float *d_data) {
@@ -59,12 +71,12 @@ __device__ void perform_calculations_inline(float *d_data) {
 
 ********************************/
 
-// Kernel launches the __device__ function on each thread
+//Kernel launches the __device__ function on each thread
 __global__ void scenario1(float *d_data) {
     perform_calculations(d_data);
 }
 
-// Function for the single thread that launches a dynamic block
+//Function for the single thread that launches a dynamic block
 __global__ void scenario2(float *d_data, int depth = 0) {
     int tid = threadIdx.x;
 
@@ -77,7 +89,7 @@ __global__ void scenario2(float *d_data, int depth = 0) {
 
     if (tid == 0) {
 
-        // dynamic parallelism block with 1024 threads
+        //dynamic parallelism block with 1024 threads
         float *d_temp;
         cudaMalloc(&d_temp, NUM_THREADS * sizeof(float));
         scenario2<<<NUM_BLOCKS, NUM_THREADS>>>(d_temp, 1);
@@ -87,33 +99,41 @@ __global__ void scenario2(float *d_data, int depth = 0) {
     __syncthreads();
 }
 
-// Kernel launches the inlined __device__ function on each thread
+//Kernel launches the inlined __device__ function on each thread
 __global__ void scenario3(float *d_data) {
     perform_calculations_inline(d_data);
 }
 
-// Kernel launches the __constant__ functor on each thread
+//Kernel launches the __constant__ functor on each thread
 __global__ void scenario4(float *d_data) {
 
     object(d_data);
 }
 
-// Kernel launches the __device__ functor on each thread
+//Kernel launches the __device__ functor on each thread
 __global__ void scenario5(float *d_data) {
 
     object_dev(d_data);
 }
 
+//Kernel launches the __shared__ functor on each thread
+//(ignoring the addition of 'x' so we use a member function
+//instead of a functor call)
+__global__ void scenario6(float *d_data) {
+
+    object_shared.shared(d_data);
+}
+
 int main() {
 
     //vars
-    float *h_data, *d_data1, *d_data2, *d_data3, *d_data4, *d_data5;
+    float *h_data, *d_data1, *d_data2, *d_data3, *d_data4, *d_data5, *d_data6;
 
     cudaEvent_t start_event, stop_event;
     cudaEventCreate(&start_event);
     cudaEventCreate(&stop_event);
 
-    float scenario1_time_ms, scenario2_time_ms, scenario3_time_ms, scenario4_time_ms, scenario5_time_ms;
+    float scenario1_time_ms, scenario2_time_ms, scenario3_time_ms, scenario4_time_ms, scenario5_time_ms, scenario6_time_ms;
 
     //Make memory for random task usage
     //(replace with what you want to test)
@@ -123,6 +143,7 @@ int main() {
     cudaMalloc(&d_data3, NUM_THREADS * sizeof(float));
     cudaMalloc(&d_data4, NUM_THREADS * sizeof(float));
     cudaMalloc(&d_data5, NUM_THREADS * sizeof(float));
+    cudaMalloc(&d_data6, NUM_THREADS * sizeof(float));
 
     for (int i = 0; i < NUM_THREADS; ++i) h_data[i] = 1.0f;
 
@@ -131,13 +152,15 @@ int main() {
     cudaMemcpy(d_data3, h_data, NUM_THREADS * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_data4, h_data, NUM_THREADS * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_data5, h_data, NUM_THREADS * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_data6, h_data, NUM_THREADS * sizeof(float), cudaMemcpyHostToDevice);
 
     //Warm the device up to account for potential overhead
     scenario1<<<NUM_BLOCKS, NUM_THREADS>>>(d_data1);
     scenario2<<<NUM_BLOCKS, NUM_THREADS>>>(d_data2);
     scenario3<<<NUM_BLOCKS, NUM_THREADS>>>(d_data3);
-    scenario3<<<NUM_BLOCKS, NUM_THREADS>>>(d_data4);
-    scenario3<<<NUM_BLOCKS, NUM_THREADS>>>(d_data5);
+    scenario4<<<NUM_BLOCKS, NUM_THREADS>>>(d_data4);
+    scenario5<<<NUM_BLOCKS, NUM_THREADS>>>(d_data5);
+    scenario6<<<NUM_BLOCKS, NUM_THREADS>>>(d_data6);
     cudaDeviceSynchronize();
 
     //Run Scenario 1
@@ -180,10 +203,19 @@ int main() {
 
     cudaEventElapsedTime(&scenario5_time_ms, start_event, stop_event);
 
+    //Run Scenario 6
+    cudaEventRecord(start_event, 0);
+    scenario6<<<NUM_BLOCKS, NUM_THREADS>>>(d_data6);
+    cudaEventRecord(stop_event, 0);
+    cudaEventSynchronize(stop_event);
+
+    cudaEventElapsedTime(&scenario6_time_ms, start_event, stop_event);
+
     //Print times
     std::cout << "Scenario: (1024 threads, inlined kernels) time: " << scenario3_time_ms << " ms\n";
     std::cout << "Scenario: (1024 threads, individual kernels) time: " << scenario1_time_ms << " ms\n";
     std::cout << "Scenario: (1024 threads, dynamic parallelism) time: " << scenario2_time_ms << " ms\n";
+    std::cout << "Scenario: (1024 threads, polymorphic functor mimic [shared]) time: " << scenario6_time_ms << " ms\n";
     std::cout << "Scenario: (1024 threads, polymorphic functor mimic [device]) time: " << scenario5_time_ms << " ms\n";
     std::cout << "Scenario: (1024 threads, polymorphic functor mimic [constant]) time: " << scenario4_time_ms << " ms\n";
 
